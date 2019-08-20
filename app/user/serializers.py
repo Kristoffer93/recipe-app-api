0132ace44a4095adf6a5dd0b2c8311e169a8aed7
Kristoffer Gustafsson <kristoffer@studio27.se>
2019-08-20 19:30:58 +0200

    Adding me api

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -16,6 +16,17 @@ class UserSerializer(serializers.ModelSerializer):
         """Skapar en ny användare med ett krypterat lösenord"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """updatera användaren och hämta den"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer för användar autenikation"""
